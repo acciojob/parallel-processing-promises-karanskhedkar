247ebeb51c890309e1f3d929aab83a22a4908561
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-23 11:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const downloadImage = (imageUrl) => {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = imageUrl;
+    image.onload = () => resolve(image);
+    image.onerror = () => reject("Download failed");
+  });
+};
+
+const downloadImages = () => {
+  const loadingDiv = document.createElement("div");
+  loadingDiv.setAttribute("id", "loading");
+  loadingDiv.textContent = "Loading...";
+  document.body.prepend(loadingDiv);
+
+  const downloadPromises = images.map((imageData) =>
+    downloadImage(imageData?.url)
+  );
+  Promise.all(downloadPromises)
+    .then((data) => {
+      loadingDiv.remove();
+      data.forEach((img) => {
+        output.prepend(img);
+      });
+    })
+    .catch((error) => {
+      loadingDiv.remove();
+      const errorDiv = document.createElement("div");
+      errorDiv.setAttribute("id", "error");
+      errorDiv.textContent = "Image download filed";
+      document.body.prepend(errorDiv);
+    });
+};
+
+downloadImages();
